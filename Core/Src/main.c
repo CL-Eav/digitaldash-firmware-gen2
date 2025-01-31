36982ef60e5d541a040e3f5c4f9e9f1148731f82
Matt Kaiser <MatthewKaiser45@gmail.com>
2025-01-31 00:38:52 -0500

    link lvgl

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "lvgl.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
