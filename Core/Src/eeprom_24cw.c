fc1254695483eb1334e2b23d32757a3230185456
Matt Kaiser <MatthewKaiser45@gmail.com>
2025-06-19 00:11:50 -0400

    EEPROM safe read/write

--- Core/Src/eeprom_24cw.c
+++ Core/Src/eeprom_24cw.c
@@ -7,11 +7,11 @@
 
 #include "eeprom_24cw.h"
 
-#define EEPROM_READ_DELAY_MS 5
+#define EEPROM_READ_DELAY_MS 0
 #define EEPROM_WRITE_RETRY_COUNT 10
 #define EEPROM_READ_TRIAL_COUNT 3
 #define EEPROM_READY_TIMEOUT 10
-#define EEPROM_WRITE_DELAY_MS 2
+#define EEPROM_WRITE_DELAY_MS 6
 #define EEPROM_ADDRESS_SIZE 2
 #define CONFIG_EEPROM_7BIT_ADDR 0xA0
 #define EEPROM_TIMEOUT 10
@@ -27,6 +27,10 @@ uint8_t eeprom_24cw_read(I2C_HandleTypeDef *eeprom_handle, uint16_t bAdd)
 
     // Perform I2C read operation
     HAL_I2C_Mem_Read(eeprom_handle, CONFIG_EEPROM_7BIT_ADDR, bAdd, EEPROM_ADDRESS_SIZE, rbuf, sizeof(rbuf), EEPROM_TIMEOUT);
+
+	#if EEPROM_READ_DELAY_MS > 0
+    HAL_Delay(EEPROM_READ_DELAY_MS);
+	#endif
 
     return rbuf[0];
 }
@@ -44,7 +48,9 @@ void eeprom_24cw_write(I2C_HandleTypeDef *eeprom_handle, uint16_t bAdd, uint8_t 
 	// Perform I2C write operation
 	HAL_I2C_Mem_Write(eeprom_handle, CONFIG_EEPROM_7BIT_ADDR, bAdd, EEPROM_ADDRESS_SIZE, wbuf, sizeof(wbuf), EEPROM_TIMEOUT);
 
+	HAL_Delay(EEPROM_WRITE_DELAY_MS);
 
+	/*
     // Wait until EEPROM completes the internal write cycle
     for (int retry = 0; retry < EEPROM_WRITE_RETRY_COUNT; retry++)
     {
@@ -54,5 +60,6 @@ void eeprom_24cw_write(I2C_HandleTypeDef *eeprom_handle, uint16_t bAdd, uint8_t 
         }
         HAL_Delay(EEPROM_WRITE_DELAY_MS); // Delay between retries
     }
+    */
 }
 
