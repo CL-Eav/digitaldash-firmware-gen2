8740ca0ed46c67560e10d928426a7f2a2cec7729
Matt Kaiser <MatthewKaiser45@gmail.com>
2025-07-25 14:31:25 -0400

    Add smoothing to linear

--- ui/themes/linear.c
+++ ui/themes/linear.c
@@ -24,31 +24,43 @@
 
 static void event_cb(lv_event_t * e)
 {
-	// Get the PID data
-	PID_DATA * data = (PID_DATA *)lv_event_get_param(e);
-	lv_obj_t * gauge = lv_event_get_target(e);
+    PID_DATA * data = (PID_DATA *)lv_event_get_param(e);
+    lv_obj_t * gauge = lv_event_get_target(e);
     lv_obj_t * needle = lv_obj_get_child(gauge, 1);
     lv_obj_t * span_group = lv_obj_get_child(gauge, 2);
     lv_span_t * span_val = lv_spangroup_get_child(span_group, 0);
     lv_obj_t * min = lv_obj_get_child(gauge, 3);
     lv_obj_t * max = lv_obj_get_child(gauge, 4);
 
+    // New target value
+    int32_t new_value = scale_float(data->pid_value, data->precision);
+    int32_t current_value = lv_bar_get_value(needle);
+
+    if (current_value != new_value) {
+        lv_anim_t a;
+        lv_anim_init(&a);
+        lv_anim_set_var(&a, needle);
+        lv_anim_set_values(&a, current_value, new_value);
+        lv_anim_set_duration(&a, ANIM_SPEED); // Animation duration
+        lv_anim_set_exec_cb(&a, (lv_anim_exec_xcb_t)lv_bar_set_value);
+        lv_anim_set_path_cb(&a, lv_anim_path_ease_in_out); // <- smooth animation
+        lv_anim_start(&a);
+    }
+
+    // Update needle color immediately
     lv_color_t needle_color = get_needle_color_from_value(data->pid_value, data->lower_limit, data->upper_limit);
-
-    lv_bar_set_value(needle, scale_float(data->pid_value, data->precision), LV_ANIM_OFF);
-
     lv_obj_set_style_bg_color(needle, needle_color, LV_PART_INDICATOR);
 
+    // Update text display
     char value_buf[16];
     snprintf(value_buf, sizeof(value_buf), float_only[data->precision], data->pid_value);
     lv_span_set_text(span_val, value_buf);
     lv_spangroup_refresh(span_group);
 
     if( min != NULL )
-    	lv_label_set_text_fmt(min, float_only[data->precision], data->pid_min);
-
+        lv_label_set_text_fmt(min, float_only[data->precision], data->pid_min);
     if( max != NULL )
-    	lv_label_set_text_fmt(max, float_only[data->precision], data->pid_max);
+        lv_label_set_text_fmt(max, float_only[data->precision], data->pid_max);
 }
 
 lv_obj_t * add_linear_gauge( int32_t x, int32_t y, int32_t w, int32_t h, lv_obj_t * parent, PID_DATA * pid)
@@ -74,20 +86,20 @@ lv_obj_t * add_linear_gauge( int32_t x, int32_t y, int32_t w, int32_t h, lv_obj_
 
     lv_obj_t * needle = lv_bar_create(gauge);
     lv_bar_set_range(needle, scale_float(pid->lower_limit, pid->precision), scale_float(pid->upper_limit, pid->precision));
-    lv_bar_set_mode(needle, LV_BAR_MODE_SYMMETRICAL);
-    lv_obj_set_width(needle, w - BAR_PADDING);
-    lv_obj_set_height(needle, BAR_HEIGHT);
-    lv_obj_set_x(needle, 0);
-    lv_obj_set_y(needle, BAR_Y_OFFSET);
+    lv_bar_set_mode(needle, LV_BAR_MODE_NORMAL);
+    lv_obj_set_size(needle, w - BAR_PADDING, BAR_HEIGHT);
     lv_obj_set_align(needle, LV_ALIGN_TOP_MID);
-    lv_obj_set_style_radius(needle, 5, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_outline_color(needle, lv_color_hex(0x000000), LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_outline_opa(needle, 125, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_outline_width(needle, 2, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_outline_pad(needle, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_radius(needle, 0, LV_PART_INDICATOR | LV_STATE_DEFAULT);
-    //Compensating for LVGL9.1 draw crash with bar/slider max value when top-padding is nonzero and right-padding is 0
-    if(lv_obj_get_style_pad_top(needle, LV_PART_MAIN) > 0) lv_obj_set_style_pad_right(needle, lv_obj_get_style_pad_right(needle, LV_PART_MAIN) + 1, LV_PART_MAIN);
+    lv_obj_set_y(needle, BAR_Y_OFFSET);
+
+    lv_obj_set_style_radius(needle, 0, LV_PART_MAIN);
+    lv_obj_set_style_radius(needle, 0, LV_PART_INDICATOR);
+    lv_obj_set_style_bg_opa(needle, LV_OPA_COVER, LV_PART_INDICATOR);
+    lv_obj_set_style_bg_color(needle, lv_color_hex(0x00FF00), LV_PART_INDICATOR);
+
+    lv_obj_set_style_outline_width(needle, 0, LV_PART_MAIN);
+    lv_obj_set_style_border_width(needle, 0, LV_PART_MAIN);
+    lv_obj_set_style_pad_all(needle, 0, LV_PART_MAIN);
+    lv_obj_set_style_pad_all(needle, 0, LV_PART_INDICATOR);
 
     // Create span group for value and unit
     lv_obj_t * span_group = lv_spangroup_create(gauge);
